dc92e4e3168c3fd6875c44fc724aa65eef98c2b1
Afief Dias Pambudi <afiefdiaspambudi@afiefs-Air.fritz.box>
2023-04-25 16:50:21 +0200

    add new feature, view email

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -18,6 +18,7 @@ function compose_email() {
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
+  document.querySelector('#email-view').style.display = 'none';
 
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
@@ -31,6 +32,8 @@ function load_mailbox(mailbox) {
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#email-view').style.display = 'none';
+
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
@@ -39,7 +42,7 @@ function load_mailbox(mailbox) {
   fetch(`/emails/${mailbox}`)
   .then(response => response.json())
   .then(emails => {emails.forEach(email => {
-    const element = document.createElement('div');
+    let element = document.createElement('div');
     element.innerHTML = `${email.sender}, ${email.subject}, ${email.timestamp},`;
     element.style.border = "solid";
     if (email.read == true) {
@@ -48,37 +51,63 @@ function load_mailbox(mailbox) {
     else {
       element.style.backgroundColor = "white";
     }
+    let id = email.id;
+    // element.addEventListener('click', show_email);
+    element.addEventListener('click', () => show_email(id));
     document.querySelector('#emails-view').append(element);
     })
   });
+}
+
+function show_email(id) {
+
+  // Show the email content and hide other views
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#email-view').style.display = 'block';
+
+  fetch(`/emails/${id}`)
+  .then(response => response.json())
+  .then(email => {
+    let element1 = document.createElement('p');
+    element1.innerHTML = `From: ${email.sender}<br>To: ${email.recipients}<br>Subject: ${email.subject}<br>Timestamp: ${email.timestamp}<br><br>${email.body}`;
+    document.getElementById('email-view').append(element1);
+  })
+
+  fetch(`/emails/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+        archived: true,
+    })
+  })
 }
 
 function send_email(event) {
   event.preventDefault();
 
   // Store fields from compose form 
-  const recipients = document.querySelector('#compose-recipients').value;
-  const subject = document.querySelector('#compose-subject').value;
-  const body = document.querySelector('#compose-body').value;
+  let recipients = document.querySelector('#compose-recipients').value;
+  let subject = document.querySelector('#compose-subject').value;
+  let body = document.querySelector('#compose-body').value;
 
   // Send a POST request to the URL
   fetch('/emails', {
     method: 'POST',
     body: JSON.stringify({
-      recipients: document.querySelector('#compose-recipients').value,
-      subject: document.querySelector('#compose-subject').value,
-      body: document.querySelector('#compose-body').value,
+      recipients: recipients,
+      subject: subject,
+      body: body,
     })
   })
-  .then(response => response.json().then(result => ({status: response.status, message: result})))
+  .then(response => response.json().then(result => ({status: response.status, result: result})))
   .then(obj => {
     console.log(obj.status);
-    console.log(obj.message);
+    console.log(obj.result);
     if (obj.status == 201) {
       load_mailbox('sent');
     } 
     else {
-      const error = obj.message.error;
+      let error = obj.result.error;
       alert(`${error}`);
     }
   });
